286b40e25af99d59dd0ef08d9910b338b35fee7a
Luqman Project <luqmanulhakiem7@gmail.com>
2024-04-23 00:21:11 +0700

    crud pengurus

--- app/Http/Controllers/PengurusController.php
+++ app/Http/Controllers/PengurusController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PengurusStoreRequest;
 use App\Models\Pengurus;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
